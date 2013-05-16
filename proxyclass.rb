5115216fc58efaa16648ccbe4e43e73e0c9fab1a
ivanberia <ivanberia@gmail.com>
2013-05-15 20:16:59 -0400

    Added support for ints and +,-,*.

--- proxyclass.rb
+++ proxyclass.rb
@@ -1,12 +1,9 @@
-#load "z3.rb"
+load "z3.rb"
+include Z3
 
 class ProxyClass
-	# Z3 context (todo)
-	@@context = nil
-	
-	# list of Z3 symbols
-	@@symbols = []
-	
+	attr_reader :val,:type,:sym,:name,:methods
+
 	def self.assert(varstr)
 		# varstr is some boolean expression ie "a < 3"
 		# this needs to check against Z3?
@@ -15,10 +12,12 @@ class ProxyClass
 		return eval(varstr)
 	end
 	
-	def initialize(actualVal)
+	def initialize(varName, actualVal)
 		@val = actualVal
 		# is this important?
 		@type = actualVal.class
+		@name = varName
+		Z3.initContext()
 	end
 	
 	def <(arg)
@@ -67,6 +66,47 @@ class ProxyClass
 		# pass them on to the original function call
 		@val.send(name, *args)
 	end
+
+	# func is a symbol, array_bool specifies if parameters need to be in an
+	# array for Z3, a is the other parameter to the operation
+	def z3Call(func,array_bool,x)
+		if x
+			case x
+				when ProxyClass
+					x_sym = x.sym
+				else
+					# Create Z3 constant for x depending on type
+					# ONLY INT IMPLEMENTED NOW, NO CHECKS FOR TYPE
+					x_sym = Z3.z3IntLiteral(x)
+			end
+			if array_bool
+				arr = Z3.z3Array([@sym,x_sym])
+				send(func,Z3.getCtx(),2,arr)
+			else
+				send(func,Z3.getCtx(),@sym,x_sym)
+			end
+		else
+			send(func,Z3.getCtx(),@sym)
+		end
+	end
+end
+
+class FixnumProxy < ProxyClass
+	@@methods = {:+ => [:Z3_mk_add,true],
+				:* => [:Z3_mk_mul,true],
+				:- => [:Z3_mk_sub,true]
+				}
+
+	def initialize(varName,actualVal)
+		super
+		@sym = Z3.z3IntVar(@name)
+	end
+	
+	def method_missing(name, *args)
+		super
+		self.z3Call(@@methods[name][0],@@methods[name][1],*args) if @@methods.has_key?(name)
+		@val.send(name, *args)
+	end
 end
 
 # does not work!
@@ -85,3 +125,8 @@ end
 class FalseClass
 	include BoolProxy
 end
+
+x = FixnumProxy.new('x',5)
+y = 3
+z = x + y
+Z3.printContext()
