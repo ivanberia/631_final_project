8dd8af9d2fdf51d2ce9aabfe95d90ee552620ec3
Beth McNany <beth.mcnany@gmail.com>
2013-05-15 20:20:15 -0400

    temp commit, working on asserts

--- proxyclass.rb
+++ proxyclass.rb
@@ -5,25 +5,104 @@ class ProxyClass
 	@@context = nil
 	
 	# list of Z3 symbols
-	@@symbols = []
+	@@symbols = Hash.new
 	
-	def self.assert(varstr)
-		# varstr is some boolean expression ie "a < 3"
-		# this needs to check against Z3?
-		# can get object_ids
+	def self.assert_aux(var1, var2)
+		val1 = var1
+		if var1.is_a? ProxyClass
+			puts "arg is a proxy"
+			val1 = var1.instance_variable_get("@val")
+		end
 		
-		return eval(varstr)
+		val2 = var2
+		if var2.is_a? ProxyClass
+			puts "arg2 is a proxy"
+			val2 = var2.instance_variable_get("@val")
+		end
+		
+		return [val1, val2]
 	end
 	
-	def initialize(actualVal)
+	def self.assert_equal(var1, var2)
+		puts "asserting equal hi sup"
+		val1, val2 = self.assert_aux(var1, var2)
+		
+		puts "asserting equal: ", val1 == val2
+		return (val1 == val2)
+	end
+	
+	def self.assert_lessthan(var1, var2)
+		val1, val2 = self.assert_aux(var1, var2)
+		
+		return (val1 < val2)
+	end
+	
+	def self.assert(varstr)
+		# varstr is some boolean expression with whitespace between variables ie "a < 3"
+		# important assumption that there are max two variables and /only/ comparison operators being used in assert
+		vararr = varstr.split()
+		print vararr
+		# create Z3 solver
+		#solver = Z3_mk_solver(context)
+		
+		# possible expressions: "<" "==" ">" "<=" ">="
+		# check if any symbols in varstr and replace accordingly?
+		operators = ["==", "!=", "<", "<=", ">", ">="]
+		theOp = nil
+		for op in operators
+			if vararr.member? op
+				# this operator is in the string
+				theOp = op
+			end
+		end
+		
+		sym1 = nil
+		sym2 = nil
+		for sy in @@symbols.keys
+			idx = vararr.index(sy)
+			if idx != nil
+				if idx == 0
+					sym1 = sy
+				else
+					sym2 = sy
+				end
+			end
+		end
+		
+		# actually have the solver solve things
+		#intz3 = Z3_solver_check(pointer, pointer)
+		
+		infostr = "for str '#{varstr}', operator = #{theOp}, symbols = "
+		evalstr = varstr
+		
+		if sym1 != nil
+			val1 = @@symbols[sym1]
+			infostr += "#{sym1}=#{val1}, "
+			evalstr = evalstr.sub(sym1, val1.to_s)
+		end
+		
+		if sym2 != nil
+			val2 = @@symbols[sym2]
+			infostr += "#{sym2}=#{val2}"
+			evalstr = evalstr.sub(sym2, val2.to_s)
+		end
+		puts infostr + "\n"
+		result = eval(evalstr)
+		puts "result: #{result}"
+	end
+	
+	def initialize(name, actualVal)
 		@val = actualVal
-		# is this important?
+		@name = name
+		
+		# create symbol here
+		sym = actualVal
+		@@symbols[name] = @val
+		
 		@type = actualVal.class
 	end
 	
 	def <(arg)
-		# catch
-		# this will go in the ProxyFixnum class
 		print "catching lt, self is ", @val, ", arg is ", arg, "\n"
 		v = @val
 		a = arg
